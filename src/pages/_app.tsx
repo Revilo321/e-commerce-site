4fe0c1629ff5f2f0e4290f8cb54ac3a2f3c95252
Revilo321 <oliverwii@live.dk>
2023-06-01 18:19:20 +0200

    added montserrat as font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import type { AppProps } from 'next/app';
+import { Montserrat } from 'next/font/google';
 import '@/styles/globals.css';
 import Layout from '@/components/Layout/LayoutComponent';
+
+const monts = Montserrat({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <main className={monts.className}>
+        <Component {...pageProps} />
+      </main>
     </Layout>
   );
 }
